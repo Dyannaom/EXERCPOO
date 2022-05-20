5e11ad6e86e29539331117e6dd89172c99cf3232
DADO <DADO@gft.com>
2022-05-20 14:18:34 -0300

    prepara o commit

--- src/EX7/Funcionario.java
+++ src/EX7/Funcionario.java
@@ -6,10 +6,10 @@ package EX7;
  */
 
 public abstract class Funcionario {
-    protected String nome;
+    private String nome;
     private int idade;
     private Double salario;
-    public Funcionario() {
+    private Funcionario() {
     }
     public Funcionario(String nome, int idade, Double salario) {
         this.nome = nome;
